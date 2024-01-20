f981aaaec904fb7ed778db1a25bc701906bdab49
Prasad Nair <prasad.writes@gmail.com>
2024-01-20 14:10:34 -0600

    fixing import errors

--- slit.py
+++ slit.py
@@ -5,6 +5,8 @@ from langchain_openai import OpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
 
 
 # Initialize a session state variable called disabled to False
@@ -47,3 +49,33 @@ st.session_state.processed_data = {
     "document_chunks": documents,
     "vectorstore": vectorstore,
 }
+
+
+
+# Load the Langchain chatbot
+llm = ChatOpenAI(temperature=0, max_tokens=1000, model_name="gpt-3.5-turbo")
+qa = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever())
+
+# Initialize Streamlit chat UI
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Ask your questions from PDF "):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+            st.markdown(prompt)
+if prompt:
+    result = qa({"question": prompt, "chat_history": [(message["role"], message["content"]) for message in st.session_state.messages]})
+    print(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = result["answer"]
+        message_placeholder.markdown(full_response + "|")
+        message_placeholder.markdown(full_response)
+        print(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
